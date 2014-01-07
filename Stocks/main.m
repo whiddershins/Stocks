4afc43b68b3d1a28ebb6ab07777a8f76f134c14d
James Calhoun <James.Calhoun@gmail.com>
2014-01-06 20:05:21 -0500

    Initial Commit

---
+++ Stocks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by James Calhoun on 1/6/14.
+//  Copyright (c) 2014 James Calhoun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
